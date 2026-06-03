56370568a76482b57627cf10e497cf4ac43b53e4
JOKERi454 <2375257696@qq.com>
2026-06-04 01:13:49 +0800

    Security: Remove hardcoded API key, use environment variables instead

---
+++ core/config.py
@@ -0,0 +1,8 @@
+﻿import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
+if not DEEPSEEK_API_KEY:
+    raise ValueError("请设置环境变量 DEEPSEEK_API_KEY")
